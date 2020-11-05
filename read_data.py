39a18080cf14ecd8d1f03f64596e2c2883c9cfc0
johnny-taylor <jbdtaylor@gmail.com>
2020-11-05 17:14:12 +0000

    Now can get timecorr and pdcsap data from FITS file

--- read_data.py
+++ read_data.py
@@ -1,11 +1,23 @@
 # File for importing and reading fits files from TESS mission
 
-# open file and read contents
+from astropy.timeseries import TimeSeries
+
+
+# Opening fits file with astropy
 def openfile():
-	print("Please enter file name you would like to open")
-	filename = input()
-	rawdata = open(filename, "r")
-	print (rawdata)
+	ts = TimeSeries.read('./DATA/tess2018206045859-s0001-0000000029829699-0120-s_lc.fits', format = 'kepler.fits')
+	return ts
+
+# Open FITS and assign data from it to variable stardata
+stardata = openfile()
 
 
-openfile()
+# Get time and pdcsap_flux from timeseries data
+def getdata(file):
+	timecorr = file['timecorr']
+	pdcsap_flux = file['pdcsap_flux']
+	return timecorr, pdcsap_flux
+
+#assign data from FITS to two variables
+timecorr, pdcsap_flux = getdata(stardata)
+print (timecorr), print (pdcsap_flux)
