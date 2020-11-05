d5860d7acde3307d66165709833169ac65610ce0
johnny-taylor <jbdtaylor@gmail.com>
2020-11-05 22:10:48 +0000

    Added function for scanning ./data/raw/ and creating list in ./outputs/ of files ending in '.fits' for later use.

--- read_data.py
+++ read_data.py
@@ -1,7 +1,25 @@
 # File for importing and reading fits files from TESS mission
 
+import os
 from astropy.timeseries import TimeSeries
 
+
+#Create list of files in ./data/raw/ and return list of variables
+# Will need to get rid of non-FITS files
+
+#define variables
+lightcurve_list = []
+
+def dirscan():
+	dir_contents_list = os.listdir('./data/raw')
+	for line in dir_contents_list:
+		if line[(len(line)-4):] == 'fits':
+			lightcurve_list.append(line)
+	with open('./outputs/lc_list.txt', 'w') as f:	#Write list to file, ./outputs/lc_list.txt
+		for item in lightcurve_list:
+			f.write('%s\n' % item)
+
+dirscan()
 
 # Opening fits file with astropy
 def openfile():
