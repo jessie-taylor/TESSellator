f6642607ba74c628ebceb49caa46b982e7fe4841
johnny-taylor <jbdtaylor@gmail.com>
2020-11-12 22:15:13 +0000

    Merged read_data.py from feat_full_list branch - main branch can now create full lists of lightcurve data and plot in graphs

--- read_data.py
+++ read_data.py
@@ -2,6 +2,9 @@
 
 import os
 from astropy.timeseries import TimeSeries
+from astropy.time import Time
+import matplotlib.pyplot as plt
+
 
 
 #Create list of files in ./data/raw/ and return list of variables
@@ -21,26 +24,40 @@ def dirscan():
 
 dirscan()
 
-num = 0 				#FOR COUNTING WITH TESTING
-# Opening fits files with astropy
+i = 0			# For counting during testing
+all_data = []			# Define list for all timeseries data to be stored in, corresponding to names in lightcurve_list
+# Opening fits file with astropy
 def openfile():
-	for item in lightcurve_list:
-		global num
-		num= num+1
-		print (num)
-		ts = TimeSeries.read('./DATA/raw/' + item, format = 'kepler.fits')
-	return ts
+	global i
+	for curve in lightcurve_list:
+		i = i+1
+		print(i)
+		all_data.append(TimeSeries.read('./DATA/raw/'+ curve, format = 'kepler.fits'))
+	return all_data
 
 # Open FITS and assign data from it to variable stardata
-stardata = openfile()
+all_data = openfile()
 
 
 # Get time and pdcsap_flux from timeseries data
-def getdata(file):
-	timecorr = file['timecorr']
-	pdcsap_flux = file['pdcsap_flux']
-	return timecorr, pdcsap_flux
+fluxes = []
+times = []
+def getdata(all_data):
+	for entry in all_data:
+		times.append(Time(entry['time'], format = 'jd').value)	# Using astropy.time to convert isot to jd time format, use value
+		fluxes.append(entry['pdcsap_flux'])
+	return fluxes, times
 
 #assign data from FITS to two variables
-timecorr, pdcsap_flux = getdata(stardata)
-print (timecorr), print (pdcsap_flux)
+getdata(all_data)
+
+# Function for plotting each lightcurve to a graph
+def plot_all():
+	for i in range (0, len(fluxes)):
+		plt.plot(times[i], fluxes[i])
+		plt.title(lightcurve_list[i])
+		plt.savefig('./outputs/testing1_outputs/'+lightcurve_list[i][:(len(lightcurve_list[i])-5)]+'.png')
+		plt.close()
+
+
+plot_all()
