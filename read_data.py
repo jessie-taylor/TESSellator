94a2983117527e9b40ac4333a69bdf2f19e37221
johnny-taylor <jbdtaylor@gmail.com>
2021-02-01 22:26:56 +0000

    Formatting of graphs changed; moved all calling on functions to end; commented out plot_zoomed() so it can be manually activated if needed

--- read_data.py
+++ read_data.py
@@ -21,7 +21,6 @@ def dirscan():
 		for item in lightcurve_list:
 			f.write('%s\n' % item)
 
-dirscan()
 
 
 i = 0			# For counting during testing
@@ -34,9 +33,6 @@ def openfile():
 		print(i)
 		all_data.append(TimeSeries.read('./DATA/raw/'+ curve, format = 'kepler.fits'))
 	return all_data
-
-# Open FITS and assign data from it to variable stardata
-all_data = openfile()
 
 
 # Get time and pdcsap_flux from timeseries data
@@ -56,39 +52,63 @@ def getdata(all_data):
         fluxes[i] = fluxes[i][~np.isnan(fluxes[i])]
     return fluxes, times
 
-#assign data from FITS to two variables
-fluxes, times = getdata(all_data)
 
 # Function to get DFT from data
 freqs, powers = [],[]       # Defining variables to store DFT values in
 from astropy.timeseries import LombScargle
 def dft():
     for i in range(0, len(fluxes)):                     # To go through all entries in both fluxes and times
-        frequency, power = LombScargle(times[i], fluxes[i]).autopower(method = 'auto')
+        frequency, power = LombScargle(times[i], fluxes[i]).autopower(method = 'auto', nyquist_factor=None, maximum_frequency=50, minimum_frequency=None)
         freqs.append(frequency)
         powers.append(power)
         print('in dft(), entry =', i)
     return freqs, powers
-dft()
 
 
 # Function for plotting each lightcurve to a graph
 def plot_all():
 	for i in range (0, len(fluxes)):
-		plt.plot(times[i], fluxes[i])
+		plt.figure(figsize=(20,10))
+		plt.plot(times[i], fluxes[i], linewidth=0.5)
 		plt.title(lightcurve_list[i])
-		plt.savefig('./outputs/testing1_outputs/'+lightcurve_list[i][:(len(lightcurve_list[i])-5)]+'.png')
+		plt.xlabel('Time (JD)')
+		plt.ylabel('PDCSAP flux ($e^{-}/s$')
+		plt.savefig('./outputs/testing1_outputs/'+lightcurve_list[i][:(len(lightcurve_list[i])-5)]+'.png', bbox_inches = "tight")
 		plt.close()
 
 
 # Function for plotting DFTs to graphs
 def plt_dft():
-    for i in range(0, len(fluxes)):
-        plt.plot(freqs[i], powers[i])
-        plt.title('DFT '+lightcurve_list[i])
-        plt.savefig('./outputs/testing1_outputs/'+lightcurve_list[i][:(len(lightcurve_list[i])-5)]+'_DFT.png')
-        plt.close()
-    
+	for i in range(0, len(fluxes)):
+		plt.figure(figsize=(20,10))
+		plt.plot(freqs[i], powers[i], linewidth=0.5)
+		plt.title('DFT '+lightcurve_list[i])
+		plt.xlabel('Frequency ($d^{-1}$)')
+		plt.ylabel('Power')
+		plt.savefig('./outputs/testing1_outputs/'+lightcurve_list[i][:(len(lightcurve_list[i])-5)]+'_DFT.png', bbox_inches = "tight")
+		plt.close()
 
+# Get plot zoomed in to just one day
+def plot_zoomed():
+	for i in range (0, len(fluxes)):
+		plt.figure(figsize=(20,10))
+		plt.plot(times[i][0:719], fluxes[i][0:719], linewidth=0.8)
+		plt.title(lightcurve_list[i])
+		plt.xlabel('Time (JD)')
+		plt.ylabel('PDCSAP flux ($e^{-}/s$')
+		plt.savefig('./outputs/testing1_outputs/'+lightcurve_list[i][:(len(lightcurve_list[i])-5)]+'_zoomed'+'.png', bbox_inches = "tight")
+		plt.close()
+
+dirscan()
+
+ # Open FITS and assign data from it to variable stardata
+all_data = openfile()
+
+  
+#assign data from FITS to two variables
+fluxes, times = getdata(all_data)
+
+dft()
 plot_all()
 plt_dft()
+#plot_zoomed()		# This is probably better called on when needed through the terminal with an -i arg when running ipython3
