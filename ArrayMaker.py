e16206e1ff68187d672c3786e521c3886cc2398a
jbdtaylor <jbdtaylor@gmail.com>
2022-02-09 22:00:53 +0000

    init Keplerellator commit
    setting up the branch for Kepler stuff

---
+++ ArrayMaker.py
@@ -0,0 +1,43 @@
+# Now requires a subdirectory in scripts dir named OutputArrays
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+from datetime import datetime
+CompleteArray =np.empty([480, 640, 0])
+NameArray = []  # ADDED FOR NAMING
+total_i = 0
+dir = 'binaries_training_set/'
+for subdirectories, dirs, files in os.walk(dir):
+  i = 0
+  for subdir in dirs:
+    try:
+      print('On file', total_i, '/', len(dirs))
+      i = i + 1
+      total_i = total_i + 1 #counting total number for recording
+      timehere = datetime.now() # getting time now
+      spectra = np.loadtxt(dir+subdir+'/s000000.dat')
+      NameArray.append(subdir[:-18]) # ADDED FOR NAMING
+      plt.plot(spectra[:, 0], spectra[:, 1])
+      if (np.max(spectra[:,1]) <=500):
+        plt.ylim(0,500)
+      plt.savefig('graph.png')
+      ImageArray = Image.open('graph.png')
+      image2 =ImageArray.convert('L')
+      ArrayAppender =np.asarray(image2, dtype="int32")
+      CompleteArray =np.dstack((ArrayAppender, CompleteArray))
+      plt.clf()
+    except:
+      pass
+    if i == 1000:
+      arrayname = 'OutputArrays/' + dir[:-1] + '_' + str(total_i) + '.npy' #naming array with which i it goes to from the last
+      np.save(arrayname, CompleteArray) #save array every 1000 files
+      np.save(arrayname[:-4] + '_names.npy', NameArray)  # ADDED FOR NAMING
+      CompleteArray = np.empty([480, 640, 0]) #clear array that's just been saved
+      NameArray = [] # Clearing naming array
+      i = 0 #reset number to 0
+    print('time to complete loop =', datetime.now()-timehere) #print time to loop
+    
+print(CompleteArray.shape)
+np.save('OutputArrays/' + dir[:-1] + '_' + str(total_i) + '.npy', CompleteArray) #save remaining files
+np.save('OutputArrays/' + dir[:-1] + '_' + str(total_i) + '_names.npy', NameArray)  # ADDED FOR NAMING
---
+++ running_saved_model.py
@@ -0,0 +1,74 @@
+# Program for loading and running trained model saved by other code
+# Saves output files in directory [kepmags]predictions_plotted/
+
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
+from os import walk
+from os import mkdir
+from datetime import datetime
+
+# Obtaining list of files in array directory (both iamgearrays and names)
+arraydir = './TestOutputArrays/'
+dirarrays = next(walk(arraydir), (None, None, []))[2]
+
+# Separate the arrays into names and image arrays
+data_orig_path = []
+names_path = []
+for filename in dirarrays: # Creating arrays of the file paths/names
+  if filename[-9:] == 'names.npy':
+    names_path.append(arraydir + filename)
+  else:
+    data_orig_path.append(arraydir + filename)
+
+# Loading in pretrained model
+model = tf.keras.models.load_model('./saved_classifier_model')
+
+def run_model(array):
+  # Import data and put all in one array
+  data_together = np.empty((480,640,0), dtype='float64')
+  timehere = datetime.now()
+
+  data_together = np.concatenate((data_together, np.load(array)), axis=2)
+  print('\nTime to concatenate =', datetime.now()-timehere)
+  # COMMENTED OUT BECAUSE NOT APPLICABLE TO SETS WHERE THE NAMES ARE ALL IN ONE ARRAY ALREADY
+  # SHOULD BE UNCOMMENTED IF THEY NEED PUTTING TOGETHER
+  names_together = np.empty((0), dtype='<U13')
+  namepath = array[:-4] + '_names.npy'  # defining path to corresponding name array
+  names_together = np.concatenate((names_together, list(reversed(np.load(namepath)))))
+
+
+  # Preparing data for the CNN
+  data1 = np.transpose(data_together, (2,0,1))
+  data1 = data1.reshape(-1, 480, 640, 1)
+  data1 = data1.astype('float32')
+  data1 = data1 / 255.
+
+  # Make predictions and save them to variable predictions
+  predictions = model.predict(data1)
+
+  # Output graphs with labels in filename
+  classification_labels = ['nv', 'gd', 'ds', 'hyb']
+
+  for i in range(0, len(data1)): 
+    Image.fromarray(np.transpose(data_together, (2,0,1))[i]).convert('RGB').save(
+          arraydir[:6] + '_predictions_plotted/'
+          + classification_labels[np.argmax(predictions[i])]
+          + '_'
+          + names_together[i]
+          + '.png')
+
+# Creating directory for output, unless it already exists
+try:
+  mkdir(arraydir[:6] + '_predictions_plotted/')
+except OSError:
+  pass
+
+
+# Running model across each of the available arrays
+i = 1
+for array in data_orig_path:
+  print(f'Running on array {i} of {len(data_orig_path)}')
+  run_model(array)  #run_model being the name of the function to be defined above
+  i = i + 1
