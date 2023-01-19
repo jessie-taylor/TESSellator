06d0111d06c86093917d16a489fa9bdda8f872e9
jessie-taylor <jessie.taylor@alumni.york.ac.uk>
2023-01-19 21:01:06 +0000

    Copied section from obtaining_skarka_data.py for outputting

--- DATA/KEBC/obtaining_kirk_data.py
+++ DATA/KEBC/obtaining_kirk_data.py
@@ -4,3 +4,27 @@ import pandas as pd
 csv = pd.read_csv('./KEBCv3.csv', header=7, usecols=[i for i in range(11)])
 
 # For the list of the #KICS, use csv['#KIC']
+
+skippedlist = []
+# Copied from obtaining_skarka_data.py
+# TESTING - WILL OUTPUT LK PERIODOGRAMS OF STARS - can be modified later so it saves them
+for star in csv["Name"]:
+  print ("\nSearching for star", star)
+  try:
+    print(lk.search_lightcurve("TIC" + str(star), exptime = 1800, author = "TESS-SPOC")) #seeing which sectors are available)
+    lc = lk.search_lightcurve("TIC" + str(star), exptime = 1800, author = "TESS-SPOC"
+                        ).download(  # changed to download_all for experimenting with stitching
+#                        ).stitch(        # putting multiple together
+                        ).remove_nans()
+    LombScarglePeriodogram.from_lightcurve(lc).plot() # Should add nyquist_factor arg
+    plt.ylim(0,500) # Setting y limits to same as used with Kepler data
+    plt.tick_params(axis = 'both', which = 'both', direction = 'out') # Moving ticks to outside of plot
+    plt.savefig("./eb_periodograms_delete_later/" + str(star) + ".png")
+    lc.plot()    # INSERT LINE HERE FOR PLOTTING THE LC BEFORE DFT
+    plt.savefig("./eb_periodograms_delete_later/" + str(star) + "_lc.png")
+  except AttributeError:  # Error handling if no data available, as the download fn will output AttributeError
+    print("Skipped star", star)
+    skippedlist.append(star)
+
+# Print total number of stars and those skipped due to no available data
+print( "\nTotal stars searched for:", len(csv["Name"]), "\nTotal skipped:", len(skippedlist))
