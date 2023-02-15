370b1af33ddca634fa2a15d6cd6291b5bc12abe6
jessie-taylor <jessie.taylor@alumni.york.ac.uk>
2023-02-15 23:55:28 +0000

    Added TICer.py, added error handling when no TIC found

--- DATA/KEBC/obtaining_kirk_data.py
+++ DATA/KEBC/obtaining_kirk_data.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import lightkurve as lk
 from lightkurve.periodogram import LombScarglePeriodogram
+from TICer import TICer
+import matplotlib.pyplot as plt
 
 # Getting the first 11 columns, as it imports 12 with the last one being blank 
 csv = pd.read_csv('./KEBCv3.csv', header=7, usecols=[i for i in range(11)])
-
-# For the list of the #KICS, use csv['#KIC']
 
 skippedlist = []
 # Copied from obtaining_skarka_data.py
 # TESTING - WILL OUTPUT LK PERIODOGRAMS OF STARS - can be modified later so it saves them
 for star in csv["#KIC"]:
-  print ("\nSearching for star", star)
+  kicid = str(star)
+  star = TICer(str(star))
+  print ("\nSearching for star TIC", star, ", KIC", kicid)
   try:
     print(lk.search_lightcurve("TIC" + str(star), exptime = 1800, author = "TESS-SPOC")) #seeing which sectors are available)
     lc = lk.search_lightcurve("TIC" + str(star), exptime = 1800, author = "TESS-SPOC"
@@ -24,9 +26,15 @@ for star in csv["#KIC"]:
     plt.savefig("./eb_periodograms_delete_later/" + str(star) + ".png")
     lc.plot()    # INSERT LINE HERE FOR PLOTTING THE LC BEFORE DFT
     plt.savefig("./eb_periodograms_delete_later/" + str(star) + "_lc.png")
+    plt.close()
   except AttributeError:  # Error handling if no data available, as the download fn will output AttributeError
-    print("Skipped star", star)
-    skippedlist.append(star)
+    if star == None:
+      print("Skipped star KIC ", kicid)
+      skippedlist.append("KIC " + kicid)
+    else:
+      print("Skipped star", star)
+      skippedlist.append(star)
 
 # Print total number of stars and those skipped due to no available data
 print( "\nTotal stars searched for:", len(csv["Name"]), "\nTotal skipped:", len(skippedlist))
+
