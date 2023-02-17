698946a1bbcf034c2425453a44947bf5708fa223
jessie-taylor <jessie.taylor@alumni.york.ac.uk>
2023-02-17 22:00:31 +0000

    Cleaned up and completed

--- DATA/KEBC/get_ebs.py
+++ DATA/KEBC/get_ebs.py
@@ -1,9 +1,6 @@
 # Module version of obtaining_kirk_data.py made to be run with arraymaker
 import pandas as pd
-import lightkurve as lk
-from lightkurve.periodogram import LombScarglePeriodogram
-from TICer import TICer
-import matplotlib.pyplot as plt
+from KEBC.TICer import TICer
 from time import sleep
 
 #              ~   ~
@@ -14,10 +11,8 @@ from time import sleep
 # ~ ~ ~ ~ Module version ~ ~ ~ ~ #
 def ebs():
   """
-  Uses the Kepler Eclipsing Binary Catalog to obtain lightcurves of 
-  EB stars identified in V3 of the KEBC.
-  Performs DFT on light curves before 
-  
+  Provides TIC IDs for the stars within the KEBC V3.
+   
   Yields
   ------
   Python list of TIC IDs of KEBC
@@ -25,7 +20,7 @@ def ebs():
   eb_tic_list = []
   # Getting the first 11 columns of the KEBCv3,
   # as it imports 12 with the last one being blank 
-  csv = pd.read_csv('./KEBCv3.csv', 
+  csv = pd.read_csv('./KEBC/KEBCv3.csv', 
                     header=7, 
                     usecols=[i for i in range(11)])
   
