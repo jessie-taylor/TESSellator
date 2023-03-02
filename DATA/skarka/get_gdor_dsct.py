2ec6b15fe7d18bb76a5ca92c202d77102d1e211b
jessie-taylor <jessie.taylor@alumni.york.ac.uk>
2023-03-02 00:47:26 +0000

    Added to obtain dsct and gdor classes

---
+++ DATA/skarka/get_gdor_dsct.py
@@ -0,0 +1,36 @@
+# Small module for import from arraymaker
+import parse
+
+def gdor():
+  table5 = parse.df_from_file("./skarka/table5_classifications.dat")
+
+  # Function for getting lists of desired type
+  def get_list(classification):
+    return table5[table5["VType"] == classification]
+  
+  # Calling function to obtain dfs of only desired classications
+  gdordf = get_list("GDOR")
+
+  # Create list and put TIC IDs in list
+  gdors = []
+  for tic in gdor["Name"]:
+    gdors.append(tic)
+
+  return gdors
+
+def dsct():
+  table5 = parse.df_from_file("./skarka/table5_classifications.dat")
+
+  # Function for getting lists of desired type
+  def get_list(classification):
+    return table5[table5["VType"] == classification]
+  
+  # Calling function to obtain dfs of only desired classications
+  dsctdf = get_list("DSCT")
+  
+  # Create list and put TIC IDs in list
+  dscts = []
+  for tic in dsct["Name"]:
+    dscts.append(tic)
+
+  return dscts
