ddb4cee2925ec91e8a9606eb4f13fb53e725e2f0
jessie-taylor <jessie.taylor@alumni.york.ac.uk>
2023-03-21 23:17:22 +0000

    bugfix - fixing rrlyr arrays put together

--- DATA/arraymaker.py
+++ DATA/arraymaker.py
@@ -153,9 +153,7 @@ dsct_ids = dsct()
 make_array(dsct_ids, "dsct")
 
 # see how it works with using both type a, b, and c at once (only 2 abs)
-absids = get_abs 
-rrcsids = get_rrcs
-rrlyr_ids = absids + rrcsids
+rrlyr_ids = get_abs() + get_rrcs()
 make_array(rrlyr_ids, "rrlyr")
 
 nvids = nvs()
